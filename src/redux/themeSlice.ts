f7755979896261d6f4f60c0eb6ce28c5efcf4fe5
Boaz <boaserem022@gmail.com>
2023-08-14 04:34:22 +0300

    set initial theme to system preferences

--- src/redux/themeSlice.ts
+++ src/redux/themeSlice.ts
@@ -1,25 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { ThemeInterface } from "../types/types";
+import { ThemeInterface } from '../types/types';
+
+// Determine the initial theme based on system preferences
+const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 const initialState: ThemeInterface = {
-    theme: "dark",
+  theme: prefersDarkTheme ? 'dark' : 'light',
 };
 
-//managing theme
+// Managing theme
 const themeSlice = createSlice({
-    name: "theme",
-    initialState,
-    reducers: {
-        TOGGLE_THEME: (state,action) => {
-            state.theme = action.payload.theme;
-        },
-    }
+  name: 'theme',
+  initialState,
+  reducers: {
+    TOGGLE_THEME: (state, action) => {
+      state.theme = action.payload.theme;
+    },
+  },
 });
 
-
-//make action available for all components
+// Make action available for all components
 export const { TOGGLE_THEME } = themeSlice.actions;
-//reducer to store
+
+// Reducer to store
 export default themeSlice.reducer;
-//reference to paginaton state
+
+// Reference to theme state
 export const selectTheme = (state: { theme: ThemeInterface }) => state.theme.theme;
